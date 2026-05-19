832bd0260e8a76e55a5c754c7a7952ffb0f0dc17
Muskan Mishra <muskanm2405@gmail.com>
2026-05-20 00:35:46 +0530

    set up backend MVC architecture

---
+++ Backend/controllers/authController.js
@@ -0,0 +1,48 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
+// Register User
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // Check if user exists
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    // Generate salt
+    const registerUser=async(req,res)=>{
+    const salt = await bcrypt.genSalt(10);
+    }
+
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create user
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+module.exports = {
+  registerUser,
+};
